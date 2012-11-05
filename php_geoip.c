55215db048fd4282ff9f989667349984dbcb225e
JoungKyun Kim <joungkyun@daum.net>
2012-11-05 05:27:20 +0000

    enhanced comments and set propset

--- php_geoip.c
+++ php_geoip.c
@@ -1,22 +1,19 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2004 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.0 of the PHP license,       |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_0.txt.                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author: JoungKYun.Kim <http://www.oops.org>                          |
-  +----------------------------------------------------------------------+
-
-  $Id$
-*/
+/**
+ * Project: mod_geoip :: GeoIP php exntension
+ * File:    php_geoip.c
+ *
+ * Copyright (c) 2012 JoungKyun.Kim
+ *
+ * LICENSE: GPL
+ *
+ * @category    Database
+ * @package     mod_geoip
+ * @author      JoungKyun.Kim <http://oops.org>
+ * @copyright   2012 OOPS.org
+ * @license     GPL
+ * @version     SVN: $Id$
+ * @since       File available since release 0.0.1
+ */
 
 /*
  * PHP5 GeoIP module "geoip"
