26dc844f70c4ce05da115ac013f7d8500b29bb3b
oops <oops@d8db81b0-65cc-11e0-b5f2-c231496d37c1>
2016-01-05 16:21:22 +0000

    support Exception on non-oops mode

--- trunk/test.php
+++ trunk/test.php
@@ -41,79 +41,84 @@ $searches = array ('oops.org', 'kornet.net', 'yahoo.com');
  *
  */
 
-/* open conutry database */
-$g = GeoIP_open (GEOIP_MEMORY_CACHE|GEOIP_CHECK_CACHE);
-/* open city database */
-if ( GeoIP_db_avail (GEOIP_CITY_EDITION_REV0) )
-	$gc = GeoIP_open (GEOIP_CITY_EDITION_REV0, GEOIP_INDEX_CACHE|GEOIP_CHECK_CACHE);
-/* open isp database */
-if ( GeoIP_db_avail (GEOIP_ISP_EDITION) )
-	$gi = GeoIP_open (GEOIP_ISP_EDITION, GEOIP_INDEX_CACHE|GEOIP_CHECK_CACHE);
+try {
+	/* open conutry database */
+	$g = GeoIP_open (GEOIP_MEMORY_CACHE|GEOIP_CHECK_CACHE);
+	/* open city database */
+	if ( GeoIP_db_avail (GEOIP_CITY_EDITION_REV0) )
+		$gc = GeoIP_open (GEOIP_CITY_EDITION_REV0, GEOIP_INDEX_CACHE|GEOIP_CHECK_CACHE);
+	/* open isp database */
+	if ( GeoIP_db_avail (GEOIP_ISP_EDITION) )
+		$gi = GeoIP_open (GEOIP_ISP_EDITION, GEOIP_INDEX_CACHE|GEOIP_CHECK_CACHE);
 
-if ( ! is_resource ($g) )
-	exit;
+	if ( ! is_resource ($g) )
+		exit;
 
-#echo "TYPE: " . geoip_database_info ($g) ."\n";
+	#echo "TYPE: " . geoip_database_info ($g) ."\n";
 
-foreach ( $searches as $v ) :
+	foreach ( $searches as $v ) {
+		/*
+		 * search geoip country database
+		 *
+		 * array geoip_id_by_name (geoip handle, host);
+		 * return:
+		 *      array (
+		 *              country_id,
+		 *              code,        // code of nation
+		 *              name         // name of nation
+		 *            );
+		 *
+		 * string geoip_country_code_by_name (geoip handle, host)
+		 * string geoip_country_code_by_name (geoip handle, host)
+		 *
+		 */
+		$r = geoip_id_by_name ($g, $v);
+		print_r ($r);
+
+		/* print city information
+		 *
+		 * array geoip_record_by_name (geoip handle, host)
+		 * return:
+		 *      array (
+		 *              country_code,  // country 2 digit code
+		 *              region,        // states or do (korean)
+		 *              city,          // name of city
+		 *              postal_code,   // postal code or post code
+		 *              latitude,
+		 *              longitude,
+		 *              if ( DB is GEOIP_CITY_EDITION_REV1 ) {
+		 *                  dma_code,
+		 *                  area_code,
+		 *              }
+		 *            );
+		 */
+		if ( is_resource ($gc) ) {
+			$rc = GeoIP_record_by_name ($gc, $v);
+			print_r ($rc);
+		}
+
+		/* print isp information
+		 *
+		 * string geoip_org_by_name (geoip handle, host)
+		 */
+		if ( is_resource ($gi) ) {
+			$ri = GeoIP_org_by_name ($gi, $v);
+			echo "    $ri\n";
+		}
+
+		#echo "### " . geoip_country_code_by_name ($g, $v) . "\n";
+		#echo "### " . geoip_country_name_by_name ($g, $v) . "\n";
+	}
+
 	/*
-	 * search geoip country database
-	 *
-	 * array geoip_id_by_name (geoip handle, host);
-	 * return:
-	 *      array (
-	 *              country_id,
-	 *              code,        // code of nation
-	 *              name         // name of nation
-	 *            );
-	 *
-	 * string geoip_country_code_by_name (geoip handle, host)
-	 * string geoip_country_code_by_name (geoip handle, host)
-	 *
+	 * close geoip database
 	 */
-	$r = geoip_id_by_name ($g, $v);
-	print_r ($r);
-
-	/* print city information
-	 *
-	 * array geoip_record_by_name (geoip handle, host)
-	 * return:
-	 *      array (
-	 *              country_code,  // country 2 digit code
-	 *              region,        // states or do (korean)
-	 *              city,          // name of city
-	 *              postal_code,   // postal code or post code
-	 *              latitude,
-	 *              longitude,
-	 *              if ( DB is GEOIP_CITY_EDITION_REV1 ) {
-	 *                  dma_code,
-	 *                  area_code,
-	 *              }
-	 *            );
-	 */
-	if ( is_resource ($gc) ) {
-		$rc = GeoIP_record_by_name ($gc, $v);
-		print_r ($rc);
-	}
-	
-	/* print isp information
-	 *
-	 * string geoip_org_by_name (geoip handle, host)
-	 */
-	if ( is_resource ($gi) ) {
-		$ri = GeoIP_org_by_name ($gi, $v);
-		echo "    $ri\n";
-	}
-	
-	#echo "### " . geoip_country_code_by_name ($g, $v) . "\n";
-	#echo "### " . geoip_country_name_by_name ($g, $v) . "\n";
-endforeach;
-
-
-/*
- * close geoip database
- */
-GeoIP_close ($g);
-if ( is_resource ($gc) ) GeoIP_close ($gc);
-if ( is_resource ($gi) ) GeoIP_close ($gi);
+	GeoIP_close ($g);
+	if ( is_resource ($gc) ) GeoIP_close ($gc);
+	if ( is_resource ($gi) ) GeoIP_close ($gi);
+} catch ( GeoIPException $e ) {
+	fprintf (STDERR, "%s\n", $e->getMessage ());
+	$err = preg_split ('/\r?\n/', $e->getTraceAsString ());
+	print_r ($err);
+}
 ?>
