2c52183c48981e581922dc8c74ad801d722d1f38
JoungKyun Kim <joungkyun@gmail.com>
2016-08-12 00:31:23 +0900

    fixed license and copyright

--- php_geoip_class.c
+++ php_geoip_class.c
@@ -1,15 +1,34 @@
+/*
+ * Copyright (c) 2016 JoungKyun.Kim <http://oops.org> all rights reserved
+ *
+ * This file is part of mod_geoip
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published
+ * by the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU LESSER GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 /**
  * Project: mod_geoip :: GeoIP php exntension
  * File:    php_geoip_class.c
  *
- * Copyright (c) 2015 JoungKyun.Kim
+ * Copyright (c) 2016 JoungKyun.Kim
  *
  * LICENSE: GPL
  *
  * @category    Database
  * @package     mod_geoip
  * @author      JoungKyun.Kim <http://oops.org>
- * @copyright   2015 OOPS.org
+ * @copyright   2016 OOPS.org
  * @license     GPL
  * @version     SVN: $Id$
  * @since       File available since release 0.0.1
